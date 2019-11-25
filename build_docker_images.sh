eba0d0c85e8a3f9bd2a36180b1522009d7d2a0e5
soywiz <soywiz@gmail.com>
2019-11-25 15:26:08 +0100

    More work

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -2,3 +2,4 @@
 docker build . -t soywiz/kotlin-native-win:latest
 docker build . -t soywiz/kotlin-native-win:java8
 docker build . -t soywiz/kotlin-native-win:opengl-openal
+docker build ./android -t soywiz/kotlin-native-win:android
